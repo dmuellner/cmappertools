1e01e7a72225112064a3526962f855e8d74c0d19
Daniel Müllner <daniel@danifold.net>
2020-08-16 23:20:43 +0200

    Updated Manylinux build.

--- .manylinux-install.sh
+++ .manylinux-install.sh
@@ -9,6 +9,7 @@ repoquery --list boost148-python
 ls -la /usr
 ls -la /usr/lib
 ls -la /usr/lib64
+ls -la /usr/lib64/boost148
 ln -s /usr/lib/libboost_thread-mt.so.1.48.0 /usr/lib/libboost_thread.so.1.48.0
 ln -s /usr/lib64/libboost_thread-mt.so.1.48.0 /usr/lib64/libboost_thread.so.1.48.0
 
