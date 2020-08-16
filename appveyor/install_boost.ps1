2557a4f329b8bd30bae1157fc862810294ba8baa
Daniel Müllner <daniel@danifold.net>
2020-08-16 08:46:01 +0200

    Debug message in Boost download.

--- appveyor/install_boost.ps1
+++ appveyor/install_boost.ps1
@@ -2,6 +2,7 @@ If (Test-Path C:\projects\cmappertools\boost){
     Write-Host "Boost is already installed."
 }Else{
     Write-Host "Downloading Boost..."
+    Write-Host "Donwload URL: https://ayera.dl.sourceforge.net/project/boost/boost-binaries/1.74.0/boost_1_74_0-msvc-$env:MSVC_VERSION.0-$env:PYTHON_ARCH.exe"
     (new-object net.webclient).DownloadFile("https://ayera.dl.sourceforge.net/project/boost/boost-binaries/1.74.0/boost_1_74_0-msvc-$env:MSVC_VERSION.0-$env:PYTHON_ARCH.exe", "C:\projects\cmappertools\boost.exe")
     Write-Host "Installing Boost..."
     Start-Process "C:\projects\cmappertools\boost.exe" -ArgumentList '/DIR="C:\projects\cmappertools\boost"','/SP-','/VERYSILENT','/SUPPRESSMSGBOXES','/NOCANCEL','/NORESTART' -Wait
