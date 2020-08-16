7da9bea524553bcbcd2123bbc87b5f5492651b13
Daniel Müllner <daniel@danifold.net>
2020-08-16 21:51:15 +0200

    Updated Manylinux build.

--- .manylinux-install.sh
+++ .manylinux-install.sh
@@ -25,7 +25,7 @@ for PYBIN in /opt/python/*/bin; do
         ls -la /io
         echo '''[build_ext]
 include_dirs=/usr/include/boost148
-library_dirs=/usr/lib/boost148
+library_dirs=/usr/lib/boost148,/usr/lib64/boost148
 ''' > /io/setup.cfg
         "${PYBIN}/pip" install -e /io/
     (cd /io && "${PYBIN}/python" setup.py test)
