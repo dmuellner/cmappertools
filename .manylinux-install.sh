e8a438c118a625bb857a45398362c863387280c9
Daniel Müllner <daniel@danifold.net>
2020-08-16 14:35:01 +0200

    Added shell scripts for Manylinux.

---
+++ .manylinux-install.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -e -x
+
+# Compile wheels
+for PYBIN in /opt/python/*/bin; do
+    if [[ "${PYBIN}" == *"cp27"* ]] || \
+       [[ "${PYBIN}" == *"cp35"* ]] || \
+       [[ "${PYBIN}" == *"cp36"* ]] || \
+       [[ "${PYBIN}" == *"cp37"* ]] || \
+       [[ "${PYBIN}" == *"cp38"* ]]; then
+        "${PYBIN}/pip" install numpy
+        "${PYBIN}/pip" install -e /io/
+	(cd /io && "${PYBIN}/python" setup.py test)
+        "${PYBIN}/pip" wheel /io/ -w dist/
+        rm -rf /io/build /io/*.egg-info
+    fi
+done
+
+# Bundle external shared libraries into the wheels
+for whl in dist/cmappertools*.whl; do
+    auditwheel repair "$whl" -w /io/dist/
+done
